c5c88c1d7cfbe2c2239da68df9138f92236b17c6
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-12-14 16:55:07 +0000

    Updated Product routes.js with all routes

--- src/services/product/routes.js
+++ src/services/product/routes.js
@@ -4,6 +4,7 @@ import moment from "moment";
 
 const productRouter = Router();
 
+// Create Product
 productRouter.post("/", async (req, res, next) => {
     try {
         const { name, description, brand, image_url, price, category } = req.body;
@@ -17,5 +18,55 @@ productRouter.post("/", async (req, res, next) => {
     }
 }
 )
+
+//Get All Products
+productRouter.get("/", async (req, res, next) => {
+    try {
+        const result = await pool.query("SELECT * FROM product;");
+        res.send(result.rows);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+//Get Specific Product
+productRouter.get("/:id", async (req, res, next) => {
+    try {
+        const result = await pool.query("SELECT * FROM product WHERE product_id = $1", 
+        [req.params.id]
+        );
+        if(result.rows[0]) {
+            res.send(result.rows[0]);
+        } else {
+            res.status(404).send(`Product with id ${req.params.id} not found.`);
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+//Edit Product
+productRouter.put("/:id", async (req, res, next) => {
+    try {
+        const updateStatement = Object.entries(req.body).map(([key, value]) => `${key} = '${value}'`).join(", ");
+        const updatedAt = moment().format("YYYY-MM-DD hh:mm:ss");
+        const query = `UPDATE product SET ${updateStatement}, updatedAt = ${updatedAt} WHERE product_id = ${req.params.id} RETURNING *;`;
+        const result = await pool.query(query);
+        res.send(result.rows[0]);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+//Delete Product
+productRouter.delete("/:id", async (req, res, next) => {
+    try {
+        const query = `DELETE FROM product WHERE product_id = ${req.params.id};`;
+        await pool.query(query);
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
 
 export default productRouter;
