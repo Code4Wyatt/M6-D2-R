1bca91c3700cfbd616fda400f5041e02e4e2cf52
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-12-14 15:46:40 +0000

    Updated

--- src/utils/db/create-tables.js
+++ src/utils/db/create-tables.js
@@ -2,15 +2,15 @@ import fs from "fs-extra";
 import path from "path";
 import pool from "./connect.js";
 
-console.log(process.env);
-
 const createTables = async () => {
     try {
         const filePath = path.join(process.cwd(), './src/utils/db/tables.sql');
         const fileContentAsBuffer = await fs.readFile(filePath);
         const fileContentAsString = fileContentAsBuffer.toString();
-        console.log(fileContentAsString);
-        return fileContentAsString;  
+
+        await pool.query(fileContentAsString);
+
+        console.log("Default tables are created");
         
     } catch (error) {
         console.log("Cannot read the SQL file due to following error: ", error);
