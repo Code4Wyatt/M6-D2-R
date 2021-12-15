c4efb4e08b1fcbdecfe0c3266b3ffdd5fcf7977a
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-12-15 14:36:57 +0000

    Updated product.js

--- src/utils/db/models/product.js
+++ src/utils/db/models/product.js
@@ -6,29 +6,29 @@ const {DataTypes} = s
 const Product = sequelize.define('Product', {
     id: {
         primaryKey: true,
-        autoIncrement: true,
-        type: DataTypes.INTEGER
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
     },
     name: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
     },
     category: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
     },
     description: {
         type: DataTypes.TEXT,
-        allowNull: false
+        allowNull: false,
     },
     image: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
     },
     price: {
         type: DataTypes.FLOAT,
-        allowNull: false
-
+        allowNull: false,
     }
-
 })
+
+export default Product;
