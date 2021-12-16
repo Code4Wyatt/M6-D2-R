248a071454257a8643ecc1143af04a80cf9fa98f
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-12-16 14:50:46 +0000

    Added category router

---
+++ src/services/categories/routes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { articles } from "../../data/articles.js";
+import { Article, User, Review, Category } from "../../utils/db/models/index.js";
+import { Op } from "sequelize";
+
+const categoryRouter = express.Router();
+
+categoryRouter.post("/bulk", async(req, res, next) => {
+    try {
+        const data = await Category.bulkCreate([
+            { name: "Accoustic" },
+            { name: "Electric" },
+            { name: "Bass" },
+            { name: "Equipment" },
+        ]);
+
+        res.send(data);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+categoryRouter.get("/", async(req, res, next) => {
+    try {
+        const data = await Category.findAll();
+        res.send(data);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+export default categoryRouter;
