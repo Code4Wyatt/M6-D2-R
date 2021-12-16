f89a5e8a2efc46268f0862782b5940d47a0c1551
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-12-16 14:57:18 +0000

    Updated relationship between models

--- src/utils/db/models/index.js
+++ src/utils/db/models/index.js
@@ -1,7 +1,24 @@
 import Product from "./product.js";
 import Review from "./review.js";
+import User from "./user.js";
+import Category from "./category.js";
 
+// One to many product to reviews, one product, many reviews
 Product.hasMany(Review, { onDelete: "CASCADE" });
 Review.belongsTo(Product, { onDelete: "CASCADE" });
+
+// One to many user to reviews, one user, many reviews
+User.hasMany(Review, { onDelete: "CASCADE" });
+Review.belongsTo(User, { onDelete: "CASCADE" });
+
+// Many to many products to categories, products can have many categories and vice versa
+Product.belongsToMany(Category, { 
+    through: ProductCategory,
+    onDelete: "CASCADE",
+});
+Category.belongsToMany(Product, {
+    though: ProductCategory,
+    onDelete: "CASCADE",
+});
 
 export default { Product, Review };
